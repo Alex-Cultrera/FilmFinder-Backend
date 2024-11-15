3beeaaadfb857d46393628f2e57a3911a7105f3c
Alex-Cultrera <alexander.cultrera@gmail.com>
2024-11-14 19:23:46 -0500

    initial commit

---
+++ src/test/java/com/codercultrera/FilmFinder_Backend/FilmFinderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codercultrera.FilmFinder_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmFinderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
